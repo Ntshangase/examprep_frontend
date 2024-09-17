ce02f04649749d10620a4e21fac23308890a1c93
Ntshangase <simphiwe323i@gmail.com>
2024-09-17 20:12:35 +0200

    x

--- src/Pages/Admin/adminLanding.js
+++ src/Pages/Admin/adminLanding.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 export default function AdminLanding() {
   return (
     <div>
-        <h1>Hello Admin</h1>
+      <h1>Hello Admin</h1>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
