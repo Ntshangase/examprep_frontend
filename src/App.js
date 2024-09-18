a9595eb6e002ff23d2c96f5be168e4eabe243b52
Ntshangase <simphiwe323i@gmail.com>
2024-09-18 11:51:14 +0200

    fix complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
-import Admin from "./Pages/Admin/AdminLanding"
-import ModeratorDashboard from './Pages/Moderator/ModeratorDashboard';
+import ModeratorDashboard from "./Pages/Moderator/ModeratorDashboard";
 import ModerateAws from "./Pages/Moderator/ModerateAws";
 import QuestionView from "./Pages/Moderator/QuestionView";
 import Admin from "./Pages/Admin/View/AdminLanding";
 import LecturerDashboard from "./Pages/Lecturer/LecturerDashboard/LecturerDashboard";
-import './Styles/global.css';
-
+import "./Styles/global.css";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/adminLanding" element={<Admin />} />
-				<Route path="/ModerateAws" element={<ModerateAws />} />
-        <Route path="/ModeratorDashboard" element={<ModeratorDashboard />} />
-		<Route path="/QuestionView" element={<QuestionView />} />
-
 				<Route path="/AdminLanding" element={<Admin />} />
+				<Route path="/ModerateAws" element={<ModerateAws />} />
+				<Route path="/ModeratorDashboard" element={<ModeratorDashboard />} />
+				<Route path="/QuestionView" element={<QuestionView />} />
 				<Route path="/LecturerDashboard" element={<LecturerDashboard />} />
 			</Routes>
 		</Router>
