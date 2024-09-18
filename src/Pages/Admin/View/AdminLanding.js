c262c02a818bfa7e28bf8ee1bfaa439a4d965ff3
Ntshangase <simphiwe323i@gmail.com>
2024-09-18 12:10:23 +0200

    add component navbar

--- src/Pages/Admin/View/AdminLanding.js
+++ src/Pages/Admin/View/AdminLanding.js
@@ -1,20 +1,11 @@
 import React from 'react';
+import Navbar from '../../../Components/Navbar/Navbar';
 
 export default function AdminLanding() {
   return (
     <div>
+      <Navbar />
       <h1>Hello Admin</h1>
-      <form>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
     </div>
   );
 }
