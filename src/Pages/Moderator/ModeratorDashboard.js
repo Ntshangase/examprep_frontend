6147ae7f58c90779aa6aa6f716b3f6e46be43d6c
Nkosikhona <nkosikhonambandlwa1@gmail.com>
2024-09-18 17:06:05 +0200

    added image

--- src/Pages/Moderator/ModeratorDashboard.js
+++ src/Pages/Moderator/ModeratorDashboard.js
@@ -1,39 +1,23 @@
 import React from "react";
 import "./ModeratorDashboard.css";
-
+import Navbar from "../../Components/Navbar/Navbar";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 
 const AWSBadge = `${process.env.PUBLIC_URL}/assets/AWS-Cloud-Practitioner-Badge.jpeg`;
 const CompTIABadge = `${process.env.PUBLIC_URL}/assets/CompTIA-A+-Badge.jpg`;
 const MicrosoftBadge = `${process.env.PUBLIC_URL}/assets/Microsoft-Fundamentals-Badge.png`;
 const AWSArchitectBadge = `${process.env.PUBLIC_URL}/assets/AWS-Solutions-Architect-Badge.png`;
-const HuaweiBadge = `${process.env.PUBLIC_URL}/assets/imgs/Huawei-HCNA-Badge.jpg`;
-
+const HuaweiBadge = `${process.env.PUBLIC_URL}/assets/Huawei-HCNA-Badge.jpg`;
 
 const ModeratorDashboard = () => {
   return (
     <div className="exam-prep-container">
       {/* Sidebar Navigation */}
-      <div className="sidebar">
-        <div className="profile-section">
-          <div className="profile-picture">
-            <img
-              src="https://via.placeholder.com/100" // Placeholder image for user profile
-              alt="User Profile"
-            />
-          </div>
-        </div>
-
-        <h3>ExamPrep Project</h3>
-        <ul>
-          <li>Select Questions</li>
-          <li>LogOut</li>
-        </ul>
-      </div>
-
+<Sidebar/>
       {/* Main Content Area */}
       <div className="main-content">
         {/* Profile Section */}
-
+        <Navbar/>
         <h2>Select Course to Moderate</h2>
         {/* Certification Badges with Labels */}
         <div className="badge-section">
