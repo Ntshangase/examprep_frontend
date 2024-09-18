2236672cf0b38fa9e8465a88a8a7ac9b2953c575
MSNDLELA <msizi.ndlela5@gmail.com>
2024-09-18 03:32:06 +0200

    implemented LecturerDashboard screen and added Navbar,Sidebar components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Admin from "./Pages/Admin/AdminLanding"
 import Testing from "./Pages/Admin/Testing";
+import LecturerDashboard from "./Pages/Lecturer/LecturerDashboard/LecturerDashboard";
+import './Styles/global.css';
+
 
 function App() {
 	return (
@@ -10,6 +13,7 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/AdminLanding" element={<Admin />} />
 				<Route path="/Testing" element={<Testing />} />
+				<Route path="/LecturerDashboard" element={<LecturerDashboard />} />
 			</Routes>
 		</Router>
 	);
