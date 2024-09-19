f84bf24d00499da4bf6af24e876e03e2970c860b
Ntshangase <simphiwe323i@gmail.com>
2024-09-19 20:32:22 +0200

    editCourse and ManageExam pages

---
+++ src/Pages/Admin/EditCourse.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './EditCourse.css'
+
+export default function EditCourse() {
+  return (
+    <div>EditCourse</div>
+  )
+}
---
+++ src/Pages/Admin/ManagaExam.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ManageExam.css'
+
+export default function ManagaExam() {
+  return (
+    <div>ManagaExam</div>
+  )
+}
