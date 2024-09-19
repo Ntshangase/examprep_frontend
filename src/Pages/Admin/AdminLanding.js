52a770e20b67e7c202e82a5ea5057f76b5d73e0f
Ntshangase <simphiwe323i@gmail.com>
2024-09-19 20:27:10 +0200

    fix sidebar display

--- src/Pages/Admin/AdminLanding.js
+++ src/Pages/Admin/AdminLanding.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import Navbar from '../../Components/Navbar/Navbar';
-import AdminSidebar from '../../Components/Sidebar/AdminSidebar';
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import AdminSidebar from "../../Components/Sidebar/AdminSidebar";
 
 export default function AdminLanding() {
-  return (
-    <div>
-      <Navbar />
-      <AdminSidebar />
-      <h1>Welcome to Admin Dashboard</h1>
-    </div>
-  );
+	return (
+		<div className="admin-landing">
+			<Navbar />
+			<AdminSidebar />
+			<div className="Admin-body">
+				<h1>Welcome to Admin Dashboard</h1>
+			</div>
+		</div>
+	);
 }
