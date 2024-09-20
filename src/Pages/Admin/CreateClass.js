39bb148ff5012dd2fdee8b893ba72d7de5cb9d40
Ntshangase <simphiwe323i@gmail.com>
2024-09-21 00:05:03 +0200

    create class skeleton

--- src/Pages/Admin/CreateClass.js
+++ src/Pages/Admin/CreateClass.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './CreateClass.css'
+import AdminSidebar from '../../Components/Sidebar/AdminSidebar';
 
 export default function CreateClass() {
   return (
-    <div>CreateClass</div>
+    <div className='create-class-container'>
+      <AdminSidebar />
+      <div className='create-class-content'>
+        <h2>Create Class</h2>
+      </div>
+    </div>
   )
 }
