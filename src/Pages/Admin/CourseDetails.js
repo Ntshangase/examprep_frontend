04440f3f8729096410b7bea56037bc3be27728d4
Ntshangase <simphiwe323i@gmail.com>
2024-09-20 05:49:44 +0200

    x

--- src/Pages/Admin/CourseDetails.js
+++ src/Pages/Admin/CourseDetails.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import './CourseDetails.css'
-import AdminSidebar from '../../Components/Sidebar/AdminSidebar';
+import React from "react";
+import "./CourseDetails.css";
+import AdminSidebar from "../../Components/Sidebar/AdminSidebar";
+import courses from "../../Data/Courses.json";
 
 export default function CourseDetails() {
-  return (
-    <div className='course-details-container'>
-      <AdminSidebar />
-      <div>
-        <h2>Course Details</h2>
-        <div>
-          <></>
-          <p>title</p>
-        </div>
-        <h4>Active Classes</h4>
-        <div className='classese'></div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="course-details-container">
+			<AdminSidebar />
+			<div className="course-detail-content">
+				<h2>Course Details</h2>
+				<div className="course-info">
+					<img src={courses[0].image} alt={courses[0].title} />
+					<h3>{courses[0].title}</h3>
+				</div>
+				<h4>Active Classes</h4>
+				<div className="classese"></div>
+			</div>
+		</div>
+	);
 }
