7f876b7e7143422fccca6269d741c4c2804b8a96
Ntshangase <simphiwe323i@gmail.com>
2024-09-20 05:14:37 +0200

    running changes

--- src/Pages/Admin/CourseDetails.js
+++ src/Pages/Admin/CourseDetails.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './CourseDetails.css'
+import AdminSidebar from '../../Components/Sidebar/AdminSidebar';
 
 export default function CourseDetails() {
   return (
-    <div>CourseDetails</div>
+    <div className='course-details-container'>
+      <AdminSidebar />
+      <div>
+        <h2>Course Details</h2>
+        <div>
+          <></>
+          <p>title</p>
+        </div>
+        <h4>Active Classes</h4>
+        <div className='classese'></div>
+      </div>
+    </div>
   )
 }
