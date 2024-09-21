9cc865dc2d67e0ee75a36dfe52d9208a0f80d8ce
Ntshangase <simphiwe323i@gmail.com>
2024-09-21 11:35:31 +0200

    x

--- src/Pages/Admin/ApproveStudent.js
+++ src/Pages/Admin/ApproveStudent.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './ApproveStudent.css';
+import AdminSidebar from '../../Components/Sidebar/AdminSidebar';
 
 function ApproveStudent() {
   return (
-    <div>ApproveStudent</div>
+    <div className='approve-student-container'>
+        <AdminSidebar />
+        <div className='approve-student-content'>
+            <h2>Approve Students</h2>
+            <div className='approve-student-content-dody'>
+                <p>Display list of students here</p>
+                <p>Display list of students here</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
