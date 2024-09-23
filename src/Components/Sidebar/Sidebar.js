315b0ddea7872c1d9950cb8e1f425b13dbc2a308
Ntshangase <simphiwe323i@gmail.com>
2024-09-23 15:10:06 +0200

    Global Sidebar

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './Sidebar.css';
+import React from "react";
+import "./Sidebar.css";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  return (
-    <aside className="sidebar">
-      <ul>
-        <li className="active">Testing Links</li>
-      </ul>
-    </aside>
-  );
+	return (
+		<aside className="sidebar-component-container">
+      <Link to="/">
+					<img src="/assets/logo.png" alt="sidebar-logo" />
+				</Link>
+			<ul className="sidebar-content">
+				<li className="sidebar-link">
+					<Link to="/AdminLanding">Home</Link>
+				</li>
+				<li className="sidebar-link">
+					<Link to="/ManageCourse">Manage Courses</Link>
+				</li>
+				<li className="sidebar-link">
+					<Link to="/ManageClass">Manage Class</Link>
+				</li>
+				<li className="sidebar-link">
+					<Link to="/ManageStudents">Manage Students</Link>
+				</li>
+			</ul>
+		</aside>
+	);
 };
 
 export default Sidebar;
