6ded38da84be48f47298d9e22ab164f3958e7302
Ntshangase <simphiwe323i@gmail.com>
2024-09-26 18:51:46 +0200

    testgenerate page

--- src/Pages/Lecturer/TestGeneratePage/TestGeneratePage.js
+++ src/Pages/Lecturer/TestGeneratePage/TestGeneratePage.js
@@ -28,6 +28,10 @@ const domains = [
 ];
 
 const TestGeneratePage = () => {
+  const [testName, setTestName] = useState('');
+  const [dueDate, setDueDate] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [totalGrade, setTotalGrade] = useState('');
   const [selectedTopics, setSelectedTopics] = useState({});
   const [totalWeight, setTotalWeight] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -53,128 +57,127 @@ const TestGeneratePage = () => {
     setTotalWeight(total);
   };
 
-  const handleGenerateTest = () => {
+  const handleGenerateTest = (e) => {
+    e.preventDefault(); // Prevent default form submission
     setIsModalOpen(true);
   };
 
   const handleStartTest = () => {
     setIsModalOpen(false);
-    navigate('/ViewClass/1', { state: { selectedTopics } });
+    const testData = {
+      testName,
+      dueDate,
+      instructions,
+      totalGrade,
+      selectedTopics,
+      totalWeight,
+    };
+    console.log(testData); // Log all test data for submission
+    navigate('/ViewClass/1', { state: { selectedTopics } }); // Navigate to the next page
   };
 
-  //1st page
-  const [testName, setTestName] = useState('');
-  const [dueDate, setDueDate] = useState('');
-  const [instructions, setInstructions] = useState('');
-  const [totalGrade, setTotalGrade] = useState('');
-
   const handleSubmit = (e) => {
-      e.preventDefault();
-      // Handle form submission logic here
-      console.log({ testName, dueDate, instructions, totalGrade });
-
-      // Reset form fields after submission
-      setTestName('');
-      setDueDate('');
-      setInstructions('');
-      setTotalGrade('');
+    e.preventDefault();
+    // Handle form submission logic here
+    handleGenerateTest(e); // Call the function to generate the test
   };
 
   return (
     <div className="dashboard">
       <div className="dashboard-content">
-        <div className='test-generate-page-half1'>
-          <form onSubmit={handleSubmit}>
-                {/* Test Name */}
-                <div className="test-generate-page-form-group">
-                    <label>Test Name</label>
-                    <input
-                        type="text"
-                        value={testName}
-                        onChange={(e) => setTestName(e.target.value)}
-                        placeholder="Enter test name"
-                        required
-                    />
-                </div>
-
-                {/* Due Date */}
-                <div className="test-generate-page-form-group">
-                    <label>Due Date</label>
-                    <input
-                        type="date"
-                        value={dueDate}
-                        onChange={(e) => setDueDate(e.target.value)}
-                        required
-                    />
-                </div>
-
-                {/* Instructions */}
-                <div className="test-generate-page-form-group">
-                    <label>Instructions</label>
-                    <textarea
-                        value={instructions}
-                        onChange={(e) => setInstructions(e.target.value)}
-                        placeholder="Enter instructions for the test"
-                        required
-                    />
-                </div>
-
-                {/* Total Grade */}
-                <div className="test-generate-page-form-group">
-                    <label>Total Grade (Marks)</label>
-                    <input
-                        type="number"
-                        value={totalGrade}
-                        onChange={(e) => setTotalGrade(e.target.value)}
-                        placeholder="Enter total grade"
-                        required
-                    />
-                </div>
-            </form>
-        </div>
-        <div className="main-content">
-          <div className="test-container">
-            <h1>Select Domains to Generate Test</h1>
-            {domains.map((domain) => (
-              <div key={domain.title} className="domain-section">
-                <h2>{domain.title}</h2>
-                {domain.topics.map((topic) => (
-                  <div key={topic} className="topic-row">
-                    <label>{topic}</label>
-                    <input
-                      type="number"
-                      min="0"
-                      placeholder="Enter questions"
-                      value={selectedTopics[domain.title]?.[topic] || ''}
-                      onChange={(e) => handleTopicChange(domain.title, topic, e.target.value)}
-                    />
-                  </div>
-                ))}
-              </div>
-            ))}
-
-            <div className="total-weight">
-              <span>Total Weight for the Test:</span>
-              <input className="total-weight-input" type="text" value={totalWeight} readOnly />
-
-              <button className="generate-button" onClick={handleGenerateTest}>
-              Create Test
-            </button>
+        <form onSubmit={handleSubmit} className='test-generate-page-form'>
+          {/* Test Configuration */}
+          <div className='test-generate-page-half1'>
+            <h2>Create Test</h2>
+            <div className="test-generate-page-form-group">
+              <label>Test Name</label>
+              <input
+                type="text"
+                value={testName}
+                onChange={(e) => setTestName(e.target.value)}
+                placeholder="Enter test name"
+                required
+              />
             </div>
 
+            <div className="test-generate-page-form-group">
+              <label>Due Date</label>
+              <input
+                type="date"
+                value={dueDate}
+                onChange={(e) => setDueDate(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="test-generate-page-form-group">
+              <label>Instructions</label>
+              <textarea
+                value={instructions}
+                onChange={(e) => setInstructions(e.target.value)}
+                placeholder="Enter instructions for the test"
+                required
+              />
+            </div>
+
+            <div className="test-generate-page-form-group">
+              <label>Total Grade (Marks)</label>
+              <input
+                type="number"
+                value={totalGrade}
+                onChange={(e) => setTotalGrade(e.target.value)}
+                placeholder="Enter total grade"
+                required
+              />
+            </div>
           </div>
 
-          {isModalOpen && (
-            <div className="modal-overlay">
-              <div className="modal-box">
-                <p>Click Okay to start the test</p>
-                <button className="modal-button" onClick={handleStartTest}>
-                  Okay
-                </button>
+          {/* Test Questions */}
+          <div className="main-content">
+            <h2>Select Domains to Generate Test</h2>
+            <div className="test-container">
+              {domains.map((domain) => (
+                <div key={domain.title} className="domain-section">
+                  <h3>{domain.title}</h3>
+                  {domain.topics.map((topic) => (
+                    <div key={topic} className="topic-row">
+                      <label>{topic}</label>
+                      <input
+                        type="number"
+                        min="0"
+                        placeholder="Enter questions"
+                        value={selectedTopics[domain.title]?.[topic] || ''}
+                        onChange={(e) => handleTopicChange(domain.title, topic, e.target.value)}
+                      />
+                    </div>
+                  ))}
+                </div>
+              ))}
+
+              <div className="total-weight">
+                <span>Total Weight for the Test:</span>
+                <input className="total-weight-input" type="text" value={totalWeight} readOnly />
               </div>
             </div>
-          )}
-        </div>
+            <button className="generate-button" type="submit">Create Test</button>
+          </div>
+
+
+          {/* Submit Button */}
+
+        </form>
+
+        {/* Modal for test confirmation */}
+        {isModalOpen && (
+          <div className="modal-overlay">
+            <div className="modal-box">
+              <p>Click Okay to start the test</p>
+              <button className="modal-button" onClick={handleStartTest}>
+                Okay
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
