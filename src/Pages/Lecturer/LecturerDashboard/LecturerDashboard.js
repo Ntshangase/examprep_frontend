c55de933cda616a53b113a3ad6a894106250d54c
Ntshangase <simphiwe323i@gmail.com>
2024-10-04 12:45:32 +0200

    bug: fixed lecture dashboard student.css styling and removed redundant css

--- src/Pages/Lecturer/LecturerDashboard/LecturerDashboard.js
+++ src/Pages/Lecturer/LecturerDashboard/LecturerDashboard.js
@@ -1,36 +1,33 @@
 import React from "react";
 import CourseCard from "../../../Components/CourseCard/CourseCard";
-import './LecturerDashboard.css';
-import courses from '../../../Data/Courses.json';  // Corrected path
+import "./LecturerDashboard.css";
+import courses from "../../../Data/Courses.json"; // Corrected path
 import Sidebar from "../../../Components/Sidebar/Sidebar";
 
 const LecturerDashboard = () => {
-  const links = [
-		{path: "/LecturerDashboard", pathName: "Home"},
-		{path: "/AddStudent", pathName: "Add Student"}
-	]
+	const links = [
+		{ path: "/LecturerDashboard", pathName: "Home" },
+		{ path: "/AddStudent", pathName: "Add Student" },
+	];
 
-    return (
-        <div className="lecturer-dashboard">
-
-        <div className="dashboard-content">
-            <Sidebar links={links}/>
-          <div className="content-area">
-            <h1>Courses</h1>
-            <div className="courses-grid">
-              {courses.map((course) => (
-                <CourseCard 
-                  key={course.id} 
-                  id={course.id}
-                  title={course.title} 
-                  image={course.image} 
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+	return (
+		<div className="lecturer-dashboard-container">
+			<Sidebar links={links} />
+			<div className="lecture-dashboard-content-area">
+				<h1 className="lecture-dashboard-h1">Courses</h1>
+        <div className="lecture-dashboard-courses-grid">
+					{courses.map((course) => (
+						<CourseCard
+							key={course.id}
+							id={course.id}
+							title={course.title}
+							image={course.image}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default LecturerDashboard;
