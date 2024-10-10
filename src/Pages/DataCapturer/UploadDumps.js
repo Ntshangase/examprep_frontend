ee6105df068491c98252c5a0ce89766923e995c5
Ntshangase <simphiwe323i@gmail.com>
2024-10-10 11:55:26 +0200

    Bug: Fixed upload dump display

--- src/Pages/DataCapturer/UploadDumps.js
+++ src/Pages/DataCapturer/UploadDumps.js
@@ -4,11 +4,8 @@ import "./UploadDumps.css";
 import courses from "../../Data/Courses.json";
 
 const UploadDumps = () => {
+	const links = [{ path: "/DataCaptureDashboard", pathName: "Home" }];
 
-	const links = [
-		{path: "/DataCaptureDashboard", pathName: "Home"}
-	]
-	
 	const [file, setFile] = useState(null);
 
 	const handleFileChange = (e) => {
@@ -28,13 +25,11 @@ const UploadDumps = () => {
 	};
 
 	return (
-		<div className="lecturer-dashboard">
-			<div className="dashboard-content">
-				<Sidebar links={links} />
-				<div className="content-area">
-					<h3 className="page-title">Upload Dump</h3>
-					<form onSubmit={handleSubmit} className="upload-form">
-					<div className="add-questions-info-flow">
+		<div className="upload-dumps-dashboard">
+			<Sidebar links={links} />
+			<div className="upload-dumps-content">
+				<h2 className="upload-dumps-h2">Upload Dump</h2>
+				<div className="add-questions-info-flow">
 					<img
 						src={courses[0].image}
 						alt={courses[0].title}
@@ -44,7 +39,9 @@ const UploadDumps = () => {
 						<h3>{courses[0].title}</h3>
 					</div>
 				</div>
-						<label htmlFor="file-input" className="file-label">
+				<form onSubmit={handleSubmit} className="upload-dump-input-area">
+					<div>
+						<label htmlFor="file-input" className="upload-dump-file-label">
 							<img
 								src="/assets/uploadfile.png"
 								alt="File icon"
@@ -60,25 +57,22 @@ const UploadDumps = () => {
 							required
 							style={{ display: "none" }}
 						/>
-						<button
-							type="button"
-							onClick={() => document.getElementById("file-input").click()}
-							className="upload-button"
-						>
-							Choose File
-						</button>
-						<br />
-						<br />
-						<br />
-						<div className="action-buttons">
-							<button type="button" className="upload-dump-quit-button">
-								Quit
-							</button>
-							<button type="submit" className="upload-dump-submit-button">
-								Upload Dump
-							</button>
-						</div>
-					</form>
+					</div>
+					<button
+						type="button"
+						onClick={() => document.getElementById("file-input").click()}
+						className="upload-button"
+					>
+						Choose File
+					</button>
+				</form>
+				<div className="action-buttons">
+					<button type="button" className="upload-dump-quit-button">
+						Quit
+					</button>
+					<button type="submit" className="upload-dump-submit-button">
+						Upload Dump
+					</button>
 				</div>
 			</div>
 		</div>
