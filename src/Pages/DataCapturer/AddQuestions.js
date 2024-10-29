ec59ddabc680e0be79eab9fc25559d9b3b6cb053
Ntshangase <simphiwe323i@gmail.com>
2024-10-29 14:22:56 +0200

    feat: adding multiple choice questions and true/false questions

--- src/Pages/DataCapturer/AddQuestions.js
+++ src/Pages/DataCapturer/AddQuestions.js
@@ -17,6 +17,14 @@ export default function AddQuestions() {
 	const [courseData, setCourseData] = useState();
 	const [loadingState, setLoadingState] = useState(true);
 	const { courseId } = useParams();
+	const [questionType, setQuestionType] = useState("");
+	const [trueCheckboxAnswer, setTrueCheckboxAnswer] = useState(false);
+	const [falseCheckboxAnswer, setFalseCheckboxAnswer] = useState(false);
+	const [
+		trueFalseCorrectAnswerDescription,
+		setTrueFalseCorrectAnswerDescription,
+	] = useState("");
+	const [trueFalseQuestion, setTrueFalseQuestion] = useState("");
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -76,13 +84,20 @@ export default function AddQuestions() {
 		setIncorrectAnswers([...incorrectAnswers, ""]);
 	};
 
+	const validateOneCheckboxIsChecked = () => {
+		if (!trueCheckboxAnswer && !falseCheckboxAnswer) {
+			alert("Please select either True or False.");
+			return; // Stop form submission
+		}
+	}
+
 	const handleSubmitMultipleChoice = async (e) => {
 		e.preventDefault();
 
 		const payloadMultipleChoice = {
 			questionText: question,
 			topicId: selectedTopic,
-			questionType: "MULTIPLE_CHOICE",
+			questionType: questionType,
 			answers: [
 				{
 					answerText: correctAnswers[0],
@@ -99,7 +114,7 @@ export default function AddQuestions() {
 		const questionData = {
 			questionDTO: JSON.stringify(payloadMultipleChoice),
 			pdfFile: null,
-		}
+		};
 
 		try {
 			await addQuestion(questionData);
@@ -108,7 +123,6 @@ export default function AddQuestions() {
 			console.log(error);
 		}
 
-
 		// Reset the form after successful submission
 		setSelectedDomain("");
 		setSelectedTopic("");
@@ -116,6 +130,50 @@ export default function AddQuestions() {
 		setCorrectAnswers([""]);
 		setIncorrectAnswers([""]);
 		setCorrectAnswerDescription("");
+	};
+
+	//trueORfalse question
+
+	const handleSubmitTrueFalse = async (e) => {
+		e.preventDefault();
+
+		const payloadTrueFalse = {
+			questionText: trueFalseQuestion,
+			topicId: selectedTopic,
+			questionType: questionType,
+			answers: [
+				{
+					answerText: document.getElementById("true-answer-text").textContent,
+					answerDescription: trueFalseCorrectAnswerDescription,
+					isCorrect: trueCheckboxAnswer,
+				},
+				{
+					answerText: document.getElementById("false-answer-text").textContent,
+					isCorrect: falseCheckboxAnswer,
+				}
+			],
+		};
+
+		const questionData = {
+			questionDTO: JSON.stringify(payloadTrueFalse),
+			pdfFile: null,
+		};
+
+		try {
+			await addQuestion(questionData);
+			alert("Question added successfully!");
+		} catch (error) {
+			console.log(error);
+		}
+
+		// Reset the form after successful submission
+		setSelectedDomain("");
+		setSelectedTopic("");
+		setTrueFalseQuestion("");
+		setFalseCheckboxAnswer(false);
+		setTrueCheckboxAnswer(false);
+		setTrueFalseCorrectAnswerDescription("");
+
 	};
 
 	if (loadingState) {
@@ -145,119 +203,295 @@ export default function AddQuestions() {
 						Upload Dump
 					</button>
 				</div>
-				<div className="add-questions-form-scrollable">
-					<form onSubmit={handleSubmitMultipleChoice}>
-						{/* Domain Selection */}
-						<div className="add-questions-form-group">
-							<label>Select Domain</label>
-							<select
-								value={selectedDomain}
-								onChange={(e) => {
-									setSelectedDomain(Number(e.target.value));
-									setSelectedTopic(""); // Reset topic when domain changes
-								}}
-							>
-								<option value="" disabled>
-									Select a domain
-								</option>
-								{domainsArray.map((domain, index) => (
-									<option key={index} value={domain.domainId}>
-										{domain.domainName}
-									</option>
-								))}
-							</select>
-						</div>
+				<div>
+					<label>Select Question Type</label>
+					<select
+						value={questionType}
+						onChange={(e) => setQuestionType(e.target.value)}
+					>
+						<option value="" disabled>
+							Select Question Type
+						</option>
+						<option value="MULTIPLE_CHOICE">Multiple Choice Question</option>
+						<option value="TRUE_FALSE">True or False</option>
+						<option value="SCENARIO_WITH_PDF">Pdf Question</option>
+					</select>
+				</div>
 
-						{/* Topic Selection */}
-						{selectedDomain && (
+				{/**Multiple choice question */}
+				{questionType === "MULTIPLE_CHOICE" && (
+					<div className="add-questions-form-scrollable">
+						<form onSubmit={handleSubmitMultipleChoice}>
+							{/* Domain Selection */}
 							<div className="add-questions-form-group">
-								<label>Select Topic</label>
+								<label>Select Domain</label>
 								<select
-									value={selectedTopic}
-									onChange={(e) => setSelectedTopic(Number(e.target.value))}
+									value={selectedDomain}
+									onChange={(e) => {
+										setSelectedDomain(Number(e.target.value));
+										setSelectedTopic(""); // Reset topic when domain changes
+									}}
 								>
 									<option value="" disabled>
-										Select a topic
+										Select a domain
 									</option>
-									{topics[selectedDomain].map((topic, index) => (
-										<option key={index} value={topic.topicId}>
-											{topic.topicName}
+									{domainsArray.map((domain, index) => (
+										<option key={index} value={domain.domainId}>
+											{domain.domainName}
 										</option>
 									))}
 								</select>
 							</div>
-						)}
 
-						{/* Question Input */}
-						<div className="add-questions-form-group">
-							<label>Question</label>
-							<textarea
-								value={question}
-								onChange={(e) => setQuestion(e.target.value)}
-								placeholder="Enter the question"
-							/>
-						</div>
-
-						{/* Dynamic Correct Answer Inputs */}
-						<div className="add-questions-form-group">
-							<label>Correct Answers</label>
-							{correctAnswers.map((answer, index) => (
-								<div key={index}>
-									<input
-										type="text"
-										value={answer}
-										onChange={(e) =>
-											handleCorrectAnswerChange(index, e.target.value)
-										}
-										placeholder={`Correct Answer ${index + 1}`}
-									/>
-									{index === correctAnswers.length - 1 && (
-										<button type="button" onClick={addCorrectAnswer}>
-											Add Correct Answer
-										</button>
-									)}
+							{/* Topic Selection */}
+							{selectedDomain && (
+								<div className="add-questions-form-group">
+									<label>Select Topic</label>
+									<select
+										value={selectedTopic}
+										onChange={(e) => setSelectedTopic(Number(e.target.value))}
+									>
+										<option value="" disabled>
+											Select a topic
+										</option>
+										{topics[selectedDomain].map((topic, index) => (
+											<option key={index} value={topic.topicId}>
+												{topic.topicName}
+											</option>
+										))}
+									</select>
 								</div>
-							))}
-						</div>
+							)}
 
-						{/* Dynamic Incorrect Answer Inputs */}
-						<div className="add-questions-form-group">
-							<label>Incorrect Answers</label>
-							{incorrectAnswers.map((answer, index) => (
-								<div key={index}>
-									<input
-										type="text"
-										value={answer}
-										onChange={(e) =>
-											handleIncorrectAnswerChange(index, e.target.value)
-										}
-										placeholder={`Incorrect Answer ${index + 1}`}
-									/>
-									{index === incorrectAnswers.length - 1 && (
-										<button type="button" onClick={addIncorrectAnswer}>
-											Add Incorrect Answer
-										</button>
-									)}
+							{/* Question Input */}
+							<div className="add-questions-form-group">
+								<label>Question</label>
+								<textarea
+									value={question}
+									onChange={(e) => setQuestion(e.target.value)}
+									placeholder="Enter the question"
+								/>
+							</div>
+
+							{/* Dynamic Correct Answer Inputs */}
+							<div className="add-questions-form-group">
+								<label>Correct Answers</label>
+								{correctAnswers.map((answer, index) => (
+									<div key={index}>
+										<input
+											type="text"
+											value={answer}
+											onChange={(e) =>
+												handleCorrectAnswerChange(index, e.target.value)
+											}
+											placeholder={`Correct Answer ${index + 1}`}
+										/>
+										{index === correctAnswers.length - 1 && (
+											<button type="button" onClick={addCorrectAnswer}>
+												Add Correct Answer
+											</button>
+										)}
+									</div>
+								))}
+							</div>
+
+							{/* Dynamic Incorrect Answer Inputs */}
+							<div className="add-questions-form-group">
+								<label>Incorrect Answers</label>
+								{incorrectAnswers.map((answer, index) => (
+									<div key={index}>
+										<input
+											type="text"
+											value={answer}
+											onChange={(e) =>
+												handleIncorrectAnswerChange(index, e.target.value)
+											}
+											placeholder={`Incorrect Answer ${index + 1}`}
+										/>
+										{index === incorrectAnswers.length - 1 && (
+											<button type="button" onClick={addIncorrectAnswer}>
+												Add Incorrect Answer
+											</button>
+										)}
+									</div>
+								))}
+							</div>
+
+							{/* Correct Answer Description */}
+							<div className="add-questions-form-group">
+								<label>Correct Answer Description</label>
+								<textarea
+									value={correctAnswerDescription}
+									onChange={(e) => setCorrectAnswerDescription(e.target.value)}
+									placeholder="Enter description for the correct answer"
+								/>
+							</div>
+
+							{/* Submit Button */}
+							<button className="add-question-button" type="submit">
+								Add Question
+							</button>
+						</form>
+					</div>
+				)}
+
+				{/**True or false question */}
+				{questionType === "TRUE_FALSE" && (
+					<div className="add-questions-form-scrollable">
+						<form onSubmit={handleSubmitTrueFalse}>
+							{/* Domain Selection */}
+							<div className="add-questions-form-group">
+								<label>Select Domain</label>
+								<select
+									value={selectedDomain}
+									onChange={(e) => {
+										setSelectedDomain(Number(e.target.value));
+										setSelectedTopic(""); // Reset topic when domain changes
+									}}
+								>
+									<option value="" disabled>
+										Select a domain
+									</option>
+									{domainsArray.map((domain, index) => (
+										<option key={index} value={domain.domainId}>
+											{domain.domainName}
+										</option>
+									))}
+								</select>
+							</div>
+
+							{/* Topic Selection */}
+							{selectedDomain && (
+								<div className="add-questions-form-group">
+									<label>Select Topic</label>
+									<select
+										value={selectedTopic}
+										onChange={(e) => setSelectedTopic(Number(e.target.value))}
+									>
+										<option value="" disabled>
+											Select a topic
+										</option>
+										{topics[selectedDomain].map((topic, index) => (
+											<option key={index} value={topic.topicId}>
+												{topic.topicName}
+											</option>
+										))}
+									</select>
 								</div>
-							))}
-						</div>
+							)}
 
-						{/* Correct Answer Description */}
-						<div className="add-questions-form-group">
-							<label>Correct Answer Description</label>
-							<textarea
-								value={correctAnswerDescription}
-								onChange={(e) => setCorrectAnswerDescription(e.target.value)}
-								placeholder="Enter description for the correct answer"
-							/>
-						</div>
+							{/* Question Input */}
+							<div className="add-questions-form-group">
+								<label>Question</label>
+								<textarea
+									value={trueFalseQuestion}
+									onChange={(e) => setTrueFalseQuestion(e.target.value)}
+									placeholder="Enter the question"
+								/>
+							</div>
 
-						{/* Submit Button */}
-						<button className="add-question-button" type="submit">
-							Add Question
-						</button>
-					</form>
-				</div>
+							{/* Dynamic Correct Answer Input */}
+							<div className="add-questions-form-group">
+								<label>Is this the Correct Answer?</label>
+								<div>
+									<input
+										type="checkbox"
+										checked={trueCheckboxAnswer}
+										onChange={(e) => {
+											setTrueCheckboxAnswer(e.target.checked);
+											setFalseCheckboxAnswer(false)}}
+									/>
+									<span id="true-answer-text">True</span>
+								</div>
+								<div>
+									<input
+										type="checkbox"
+										checked={falseCheckboxAnswer}
+										onChange={(e) => {
+											setFalseCheckboxAnswer(e.target.checked);
+											setTrueCheckboxAnswer(false)}}
+									/>
+									<span id="false-answer-text">False</span>
+								</div>
+							</div>
+
+							{/* Correct Answer Description */}
+							<div className="add-questions-form-group">
+								<label>Correct Answer Description</label>
+								<textarea
+									value={trueFalseCorrectAnswerDescription}
+									onChange={(e) =>
+										setTrueFalseCorrectAnswerDescription(e.target.value)
+									}
+									placeholder="Enter description for the correct answer"
+								/>
+							</div>
+
+							{/* Submit Button */}
+							<button className="add-question-button" onClick={validateOneCheckboxIsChecked} type="submit">
+								Add Question
+							</button>
+						</form>
+					</div>
+				)}
+
+				{/**pdf file question */}
+				{questionType === "SCENARIO_WITH_PDF" && (
+					<div className="add-questions-form-scrollable">
+						<form>
+							{/* Domain Selection */}
+							<div className="add-questions-form-group">
+								<label>Select Domain</label>
+								<select
+									value={selectedDomain}
+									onChange={(e) => {
+										setSelectedDomain(Number(e.target.value));
+										setSelectedTopic(""); // Reset topic when domain changes
+									}}
+								>
+									<option value="" disabled>
+										Select a domain
+									</option>
+									{domainsArray.map((domain, index) => (
+										<option key={index} value={domain.domainId}>
+											{domain.domainName}
+										</option>
+									))}
+								</select>
+							</div>
+
+							{/* Topic Selection */}
+							{selectedDomain && (
+								<div className="add-questions-form-group">
+									<label>Select Topic</label>
+									<select
+										value={selectedTopic}
+										onChange={(e) => setSelectedTopic(Number(e.target.value))}
+									>
+										<option value="" disabled>
+											Select a topic
+										</option>
+										{topics[selectedDomain].map((topic, index) => (
+											<option key={index} value={topic.topicId}>
+												{topic.topicName}
+											</option>
+										))}
+									</select>
+								</div>
+							)}
+
+							{/* Question Input */}
+							<div className="add-questions-form-group">
+								<label>Question</label>
+								<textarea
+									value={question}
+									onChange={(e) => setQuestion(e.target.value)}
+									placeholder="Enter the question"
+								/>
+							</div>
+						</form>
+					</div>
+				)}
 			</div>
 		</div>
 	);
