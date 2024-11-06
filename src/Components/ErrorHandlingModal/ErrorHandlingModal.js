1ec9ba0110d0b75b70ebceafe771b13cce7e9f26
Ntshangase <simphiwe323i@gmail.com>
2024-11-06 18:59:37 +0200

    Feat: Separated students based on role, STUDENT and ENROLLSTUDENT

--- src/Components/ErrorHandlingModal/ErrorHandlingModal.js
+++ src/Components/ErrorHandlingModal/ErrorHandlingModal.js
@@ -1,9 +1,29 @@
-import React from 'react'
+// Modal.js
+import React from "react";
+import PropTypes from "prop-types";
+import "./ErrorHandlingModal.css"; // CSS for the modal styling
 
-function ErrorHandlingModal() {
-  return (
-    <div>ErrorHandlingModal</div>
-  )
-}
+const ErrorHandlingModal = ({ isOpen, onClose, title, message }) => {
+	if (!isOpen) return null;
 
-export default ErrorHandlingModal
+	return (
+		<div className="error-handling-modal-overlay">
+			<div className="error-handling-modal-container">
+				<h2>{title}</h2>
+				<p>{message}</p>
+				<button className="error-handling-button" onClick={onClose}>
+					OK
+				</button>
+			</div>
+		</div>
+	);
+};
+
+ErrorHandlingModal.propTypes = {
+	isOpen: PropTypes.bool.isRequired,
+	onClose: PropTypes.func.isRequired,
+	title: PropTypes.string.isRequired,
+	message: PropTypes.string.isRequired,
+};
+
+export default ErrorHandlingModal;
